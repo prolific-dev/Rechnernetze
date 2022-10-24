f88e54397c4da6a8b52c411a6ffb0f09f094a4b6
vincent mikl <vincent.mikl@osiris-nano.eu>
2022-10-24 15:18:22 +0200

    Changes by DASILVA

--- Station.py
+++ Station.py
@@ -26,7 +26,8 @@ class Station:
             if self.busy is False:
                 customer: Customer = heapq.heappop(self.buffer)
                 self.busy = True
-                sleep(self.delay_per_item * customer.count)
+                # sleep(self.delay_per_item * customer.count)
+                sleep(self.delay_per_item * customer.count / 10) # DEBUG MODE
                 self.fertig()
 
     def fertig(self):
