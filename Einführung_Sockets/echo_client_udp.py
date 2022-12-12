470b11650a4cfba23e24d2c8c27ba6b9561707ec
prolific-dev <de391ago@htwg-konstanz.de>
2022-12-12 18:26:22 +0100

    Clean up the mess...

--- Einführung_Sockets/echo_client_udp.py
+++ Einführung_Sockets/echo_client_udp.py
@@ -1,16 +1,19 @@
 import socket
+import time
 
-Server_IP = '127.0.0.1'
+Server_IP   = '127.0.0.1'
 Server_PORT = 50000
-MESSAGE = 'Hello, World!'
-print('Sending message', MESSAGE, 'to UDP server with IP ', Server_IP, ' on Port=', Server_PORT)
+MESSAGE     = 'Hello, World!'
+
+print(f"Sending message {MESSAGE} to UDP server with IP {Server_IP} on Port {Server_PORT}")
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 sock.settimeout(10)
 sock.sendto(MESSAGE.encode('utf-8'), (Server_IP, Server_PORT))
+
 try:
     data, addr = sock.recvfrom(1024)
-    print('received message: '+data.decode('utf-8')+' from ', addr)
+    print(f"received message: {data.decode('utf-8')} from {addr}")
 except socket.timeout:
-    print('Socket timed out at',time.asctime())
+    print(f"Socket timed out at {time.asctime()}")
 
 sock.close()
--- Einführung_Sockets/echo_server_tcp.py
+++ Einführung_Sockets/echo_server_tcp.py
@@ -1,40 +1,43 @@
 import socket
 import time
 
-My_IP = '127.0.0.1'
-My_PORT = 50000
-server_activity_period=30;
+My_IP                  = '127.0.0.1' # Lokale IP
+My_PORT                = 50000       # Lokaler Port
+server_activity_period = 30          # Zeit, wie lange der Server aktiv sein soll
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((My_IP, My_PORT))
-print('Listening on Port ',My_PORT, ' for incoming TCP connections');
+print(f"Listening on Port {My_PORT} for incoming TCP connections");
 
-t_end=time.time()+server_activity_period # Ende der Aktivitätsperiode
+# Ende der Aktivitätsperiode
+t_end = time.time() + server_activity_period
 
 sock.listen(1)
 print('Listening ...')
 
-while time.time()<t_end:
+while time.time() < t_end:
     try:
         conn, addr = sock.accept()
-        print('Incoming connection accepted: ', addr)
+        print(f"Incoming connection accepted: {addr}")
         break
     except socket.timeout:
-        print('Socket timed out listening',time.asctime())
+        print(f"Socket timed out listening {time.asctime()}")
 
-while time.time()<t_end:
+while time.time() < t_end:
     try:
         data = conn.recv(1024)
-        if not data: # receiving empty messages means that the socket other side closed the socket
-            print('Connection closed from other side');
-            print('Closing ...');
+        # receiving empty messages means that the socket other side closed the socket
+        if not data: 
+            print('Connection closed from other side')
+            print('Closing ...')
             conn.close()
             break
-        print('received message: ', data.decode('utf-8'), 'from ', addr)
+        print(f"received message: {data.decode('utf-8')} from {addr}")
         conn.send(data[::-1])
     except socket.timeout:
-        print('Socket timed out at',time.asctime())
+        print(f"Socket timed out at {time.asctime()}")
 
 sock.close()
+
 if conn:
     conn.close()
